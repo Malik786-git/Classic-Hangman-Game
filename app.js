eb8dbbb86e43132e2bb7be03ad166ccaedb96605
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2022-02-20 01:39:14 +0500

    add js

---
+++ app.js
@@ -0,0 +1,11 @@
+const word = document.getElementById('word');
+const incorrectletters = document.getElementById('incorrect-letters');
+const popup = document.getElementById('popup-container');
+const finalMessage = document.getElementById('final-message')
+const playBtn = document.getElementById('play-btn');
+const notification = document.getElementById('notification-container');
+
+
+// for hangman
+const figureParts = document.querySelectorAll('.figure-part');
+
